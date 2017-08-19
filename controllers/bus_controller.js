c7de3eb15f3c758fa5b52515ccdd253ab55fa5a8
Jacob Kato <jacobkato49@yahoo.com>
2017-08-18 18:31:30 -0700

    tentative routing

---
+++ controllers/bus_controller.js
@@ -0,0 +1,42 @@
+//requiring packages
+const express = require("express");
+
+
+
+var router = express.Router();
+
+//models
+var db= require("../models");
+
+
+//Redirects to the URL derived from the specified path
+router.get("/", function(req,res){
+  res.redirect(302, "/mpkbus");
+})
+
+
+//GET--> information for all routes
+router.get("/mpkbus:", function(req,res){
+  //find me all the routes
+
+})
+
+
+//ASK CHARLIE or TYLER if I need a POST route for the app
+
+
+
+//PUT-->CREATE and UPDATE
+  /*bus info requested*/
+router.put("mpkbus/update/request/:id", function(req,res){
+
+
+})
+
+
+
+  /*update request on info*/
+router.put("mpkbus/update/return/:id", function(req,res){
+
+
+})
