b963c3f59b36d3571c2d0c6c4a1501edcaec36b3
Jacob Kato <jacobkato49@yahoo.com>
2017-08-19 11:49:24 -0700

    figuring out the data

--- scripts/gtfs.js
+++ scripts/gtfs.js
@@ -2,16 +2,29 @@ var GTFS = require("gtfs-sequelize");
 
 var pgConfig = {
   database: "server=localhost;uid=root;pwd=password;database=throw;",
-  downloadsDir: "downloads",
-  gtfsFilename: "/c/Users/Jacob Kato/Desktop/web_dev_ucla/MPK-SpiritBus/data/Improved_GTFS.zip",
+  // downloadsDir: "downloads",
+  gtfsFilename: "C:\Users\Jacob Kato\Desktop\web_dev_ucla\MPK-SpiritBus\scripts\downloads\google_transit.zip",
   spatial: true,
   sequelizeOptions: {
-    logging: false,
-    schema: "throw"
+    logging: true
   }
-}
+};
 
 var gtfs = GTFS(pgConfig);
-gtfs.loadGtfs(function() {
+gtfs.loadGtfs(function(a1, a2, a3) {
+  debugger;
   //database loading has finished callback
 });
+
+
+// var GTFS = require("gtfs-sequelize");
+//
+// var downloadConfig = {
+//   gtfsUrl: "http://transitfeeds.com/p/spirit-bus/757/google_transit.zip",
+//   downloadsDir: "downloads"
+// };
+
+// var gtfs = GTFS(downloadConfig);
+// gtfs.downloadGtfs(function() {
+//     //download has finished callback
+// });
