88099191b8be3b172b20eb90b6fe6d8a637039d6
Jacob Kato <jacobkato49@yahoo.com>
2017-08-18 20:00:57 -0700

    adding more packages and starting the server

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 const express= require("express");
 const bodyParser = require("body-parser");
 const methodOver = require("method-override");
+const expressHandle = require("express-handlebars");
+const expressSess = require("express-session")
 
+
+//use the models
+const db= require("./models");
 
 //the app is using express
 const app = express();
@@ -12,6 +17,23 @@ const PORT = process.env.PORT || 3000;
 
 
 
-app.listen(PORT, function(){
-  console.log("Listening on PORT: " + PORT);
+//bodyParser - parsing responses (middleware)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+
+//express handlebars area
+app.engine("handlebars", expressHandle({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+
+//syncing the models to sequelize
+db.sequelize.sync({}).then(function() {
+  //listening to this port when we spin up the server
+  app.listen(PORT, function(){
+    console.log("Listening on PORT: " + PORT);
+  })
 })
