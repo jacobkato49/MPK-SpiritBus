76424ff3e338fb915b9d2c7e1622da87f9a9ec77
Daniel Lo Bue <dtlobue@gmail.com>
2017-08-23 18:43:27 -0700

    first attempt at sequelize route

--- controllers/bus_controller.js
+++ controllers/bus_controller.js
@@ -20,8 +20,15 @@ router.get("/", function(req,res){
 //GET--> information for all routes
 router.get("/mpkbus:", function(req,res){
   //find me all the routes
+  db.mpkBus.findAll({
+    where: {
+      status: {
+        $not: false,
+      }
+    }
+  })
 
-})
+});
 
 
 //ASK CHARLIE or TYLER if I need a POST route for the app
@@ -30,7 +37,7 @@ router.get("/mpkbus:", function(req,res){
 
 //PUT-->CREATE and UPDATE
   /*bus info requested*/
-router.put("mpkbus/update/request/:id", function(req,res){
+// router.put("mpkbus/update/request/:id", function(req,res){
 
 
 })
@@ -38,7 +45,7 @@ router.put("mpkbus/update/request/:id", function(req,res){
 
 
   /*update request on info*/
-router.put("mpkbus/update/return/:id", function(req,res){
+// router.put("mpkbus/update/return/:id", function(req,res){
 
 
 })
