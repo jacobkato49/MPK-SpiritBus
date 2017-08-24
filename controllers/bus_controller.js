7d71ed045082cbee54e9270d213a54bee49d48e6
Daniel Lo Bue <dtlobue@gmail.com>
2017-08-24 07:29:37 -0700

    second attempt at routing

--- controllers/bus_controller.js
+++ controllers/bus_controller.js
@@ -11,6 +11,9 @@ var router = express.Router();
 //models
 var db= require("../models");
 
+//setup Exress router to serve static files (?)
+//router.use(express.static(path.join(__dirname, 'public')));
+
 //Redirects to the URL derived from the specified path
 router.get("/", function(req,res){
   res.redirect(302, "/mpkbus");
@@ -22,16 +25,20 @@ router.get("/mpkbus:", function(req,res){
   //find me all the routes
   db.mpkBus.findAll({
     where: {
-      status: {
-        $not: false,
-      }
+      agency_id: 509
     }
+  }).then(busRoutes => {
+    // log to console
+    console.log(busRoutes);
+    // deploy to map (add routes visually to Google Map)
+    var newVariable = { }
   })
 
 });
 
 
 //ASK CHARLIE or TYLER if I need a POST route for the app
+// I think we're going to need a POST route. I'll work on it - Dan
 
 
 
